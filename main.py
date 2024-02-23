e954beb6f454208f151e3a5781ab97e895b58e0c
Gib mir 3€ <redstoneboy@outlook.de>
2024-02-23 18:07:17 +0100

    .

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ import pygame, random
 
 pygame.init()
 
-class field: v=0
+class field: v=100
+#100 field thats hidden
+#200 field thats marke
+#300 field thats showsn
 
 AREA=[16,16]
 MINES=50
@@ -12,6 +15,12 @@ fields = [[field() for i in range(AREA[1])] for j in range(AREA[0])]
 def getRandomField():
     global AREA, fields
     return fields[random.randint(0, AREA[0])][random.randint(0, AREA[1])]
+
+while miness<MINES:
+    f = getRandomField()
+    if str(f.v)=="100":
+        f.v=110
+        miness+=1
 
 window=w=pygame.display.set_mode((200,200))
 
